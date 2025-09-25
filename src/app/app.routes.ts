578432620815dd5b624419cdf2eb0e4831f73ef0
Moreira <valdosm.moreira@gmail.com>
2025-09-25 20:03:45 -0300

    criando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Cadastro } from './cadastro/cadastro';
+import { Consulta } from './consulta/consulta';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: 'cadastro', pathMatch: 'full', component: Cadastro
+},
+{
+  path: 'consulta', pathMatch: 'full', component: Consulta
+}
+];
